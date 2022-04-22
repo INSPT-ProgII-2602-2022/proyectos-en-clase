d06b76e608f73b182d2f58163a02eb24ea435be3
Charly Cimino <carlosecimino@gmail.com>
2022-04-22 18:17:09 -0300

    Primera clase

---
+++ src/proyectos/holamundo/Principal.java
@@ -0,0 +1,22 @@
+package proyectos.holamundo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Charly Cimino
+ * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
+ * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingresa tu nombre: ");
+        String nombre = input.nextLine();
+        
+        System.out.println("Bienvenid@ " + nombre);
+    }
+
+}
